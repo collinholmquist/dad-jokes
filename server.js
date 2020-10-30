20ac01de2d33df78a962b2074d06e5e4dddd6427
Collin Holmquist <collinholmquist@gmail.com>
2020-10-29 23:26:16 -0500

    altered server

--- server.js
+++ server.js
@@ -3,22 +3,31 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 
-connectionString = 'mongodb+srv://dadjokes:<password>@cluster0.jzetn.mongodb.net/<dbname>?retryWrites=true&w=majority'
+connectionString = 'mongodb+srv://dadjokes:uBzL38r@cluster0.jzetn.mongodb.net/dad-jokes?retryWrites=true&w=majority'
 
-MongoClient.connect
+MongoClient.connect(connectionString, {useUnifiedTopology: true})
+.then(client => 
+    {console.log('connected to database')
+    const db = client.db('dad-jokes')
+    
+    app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(bodyParser.urlencoded({extended: true}))
+    app.listen(3000, function(){
+        console.log("listening...")
+    })
 
-app.listen(3000, function(){
-    console.log("listening...")
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html')
+    })
+
+    app.post('/jokes', (req, res) => {
+        console.log(req.body)
+    })
+
+
 })
+.catch(error => console.error(error))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
 
-app.post('/jokes', (req, res) => {
-    console.log(req.body)
-})
 
 
